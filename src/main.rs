06dfe3d792eab9a40851c0ee5c4482654ddcebc9
JOHNATHAN DOMINGUEZ CHINO (4229084) <jdominguezchino0563@student.phoenixunion.org>
2023-02-23 09:35:01 -0700

    Made a solar system program. just beginner rust project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+
+const SUN:i32 = 1;
+const BLACK_HOLE:i32 = 1;
+const HOLE: i32 = 3;
+
+
 fn main() {
-    println!("Hello, world!");
+    let planet = 9;
+    println!("There are multiple bodies in this solar system.");
+    println!("there are {} sun(s), {} Planet(s) and {} singularity.", SUN, planet, BLACK_HOLE);
+    println!("Ever since the black hole entered the solar system, it has eaten {} plant(s).", eat_sum(planet, HOLE, BLACK_HOLE));
+    let planet = planet - eat_sum(planet, HOLE, BLACK_HOLE);
+    println!("There are now {} planets left",planet)
+
+}
+
+fn eat_sum(a: i32, b: i32,c: i32) -> i32 {
+    a - b - c
 }
